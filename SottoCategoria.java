c69b33df6135cb2c2e3518aed5c850d97bdb9bc5
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-27 21:02:19 +0200

    Add files via upload

--- SottoCategoria.java
+++ SottoCategoria.java
@@ -3,74 +3,29 @@ package dominio.parte2.punto5;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import utility_2.SottoCategoriaException;
-
-public class SottoCategoria extends Categoria implements Serializable
+public class SottoCategoria extends Contenitore implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 
-	private String nomeSottoC;
-    
     public static final String DESCRIZIONE_SOTTOCATEGORIA = "Nome sottocategoria: %s\n\t";
     public static final String ELENCO_RISORSE_VUOTO = "Al momento non sono presenti risorse\n";
     public static final String INTESTAZIONE_RISORSE = "Risorse in essa contenute:\n";
    
     public SottoCategoria(String ns)
     {
-    	super();
-    	this.nomeSottoC= ns;
+    	super(ns);
     	inizializzaElencoRisorse();
     }
     
-    public String getNome()
+    public ArrayList <Risorsa> ricercaRisorsa(IRicercaStrategy ricerca)
     {
-    	return nomeSottoC;
+    	return ricerca.ricercaRisorsa(getElencoRisorse()); 
     }
-    
-    public int getNumeroMaxGiorniPrestito()
-	{
-    	throw new SottoCategoriaException();
-	}
-	   
-	public int getNumeroMaxGiorniProroga()
-	{
-		throw new SottoCategoriaException();   
-	}
-	
-	public int getNumeroGiorniRichiestaProroga()
-	{
-	    throw new SottoCategoriaException();  
-	}
-	   
-	public int getNumeroMaxRisorseInPrestito()
-	{
-		throw new SottoCategoriaException(); 
-	}
-	
-    public void inizializzaElencoSottoCategorie()
-    {
-		throw new SottoCategoriaException(); 
-    }
-	
-	public ArrayList <SottoCategoria> getElencoSottoCategorie() 
-	{
-		throw new SottoCategoriaException();
-	}
-    
-    public void aggiungiSottoCategoria(SottoCategoria sc)
-	{
-    	throw new SottoCategoriaException();
-	}
-    
-    public boolean verificaPresenza(String t)
-	{
-    	throw new SottoCategoriaException();
-	}
     
     public String toString()
     {
  	   StringBuffer ris = new StringBuffer();
- 	   ris.append(String.format(DESCRIZIONE_SOTTOCATEGORIA, nomeSottoC));
+ 	   ris.append(String.format(DESCRIZIONE_SOTTOCATEGORIA, getNome()));
  	   
  	   if(getElencoRisorse().size() == 0)
  		   ris.append(ELENCO_RISORSE_VUOTO);
