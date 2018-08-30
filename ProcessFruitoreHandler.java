7a59df0fe9ad6120acfc58de92474191ea2b553c
rk59W18a <42214911+rk59W18a@users.noreply.github.com>
2018-08-30 18:17:41 +0200

    Add files via upload

--- ProcessFruitoreHandler.java
+++ ProcessFruitoreHandler.java
@@ -194,12 +194,77 @@ public class ProcessFruitoreHandler extends ProcessHandler implements Serializab
 		System.out.println(f.prestitiInCorso(getArchivioPrestiti()));
 	}
 
+
+	public void registraPrestitoSenzaSottoCategorie(Fruitore f, Categoria c, Risorsa r, Prestito nuovo) {
+  	  
+		if(c.getElencoRisorse().size() != Costanti.VUOTO)  
+	    {
+  	    	     System.out.printf(Costanti.CONTENUTO_CAT_RISORSA, c.getNome(), stampaElencoRisorse(c));
+	      	 
+  	    	     if(InputDati.leggiUpperChar(Costanti.INS_PROCEDERE_PRESTITO, "SN") == 'S')
+	    	         {
+  	    	        int num = InputDati.leggiIntero(Costanti.INS_NUMERO_RISORSA_PRESTITO, Costanti.NUM_MINIMO, c.getElencoRisorse().size());
+	                r = c.getElencoRisorse().get(num-Costanti.NUM_MINIMO);
+	    	
+	                if(getArchivioPrestiti().controlloDisponibilitaRisorsa(r) && getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername()) && !(getArchivioPrestiti().verificaPresenza(r, f.getUsername())))
+	   	                {
+	   		               nuovo = new Prestito(c, f, r);
+	   		               f.registraNuovoPrestito(getArchivioPrestiti(), nuovo);
+	   		               as.getPrestitiStorici().aggiungiPrestito(nuovo);
+	    	               System.out.println(Costanti.OP_SUCCESSO);
+	   	                }
+	   	                else if(!(getArchivioPrestiti().controlloDisponibilitaRisorsa(r)))
+	                          System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_1);
+                   else if(!(getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername())))
+	                              System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_2);
+                   else  
+           	              System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_3);
+	    	      }
+	    	} 
+  	    else
+  	    	    System.out.printf(Costanti.CONTENUTO_ELENCO_RISORSE_CAT_VUOTO, c.getNome());
+	}
+	
+	public void registraPrestitoConSottoCategorie(Fruitore f, Categoria c, Risorsa r, Prestito nuovo, SottoCategoria sc) {
+   		
+		System.out.printf(Costanti.CONTENUTO_CAT_SOTTO, c.getNome(), stampaElencoSottocategorie(c));
+		int num2 = InputDati.leggiIntero(Costanti.INS_NUMERO_SOTTOC_PRESTITO, Costanti.NUM_MINIMO, (c.getElencoSottoCategorie()).size());
+	    sc = (c.getElencoSottoCategorie()).get(num2-Costanti.NUM_MINIMO);
+	    	    	    	    	    
+	    if((sc.getElencoRisorse()).size() != Costanti.VUOTO)
+        {
+           System.out.printf(Costanti.CONTENUTO_SOTTO, sc.getNome(), stampaElencoRisorse(sc));
+           
+           if(InputDati.leggiUpperChar(Costanti.INS_PROCEDERE_PRESTITO, "SN") == 'S')
+           {
+              int num = InputDati.leggiIntero(Costanti.INS_NUMERO_RISORSA_PRESTITO, Costanti.NUM_MINIMO, sc.getElencoRisorse().size());
+       	      r = sc.getElencoRisorse().get(num-Costanti.NUM_MINIMO);
+       
+       	      if(getArchivioPrestiti().controlloDisponibilitaRisorsa(r) && getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername()) && !(getArchivioPrestiti().verificaPresenza(r, f.getUsername())))
+              {
+	   		         nuovo = new Prestito(c, f, r);
+	   		         f.registraNuovoPrestito(getArchivioPrestiti(), nuovo);
+	   		         as.getPrestitiStorici().aggiungiPrestito(nuovo);
+      	         System.out.println(Costanti.OP_SUCCESSO);
+              }
+              else if(!(getArchivioPrestiti().controlloDisponibilitaRisorsa(r)))
+      	                System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_1);
+              else if(!(getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername())))
+    	                System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_2);
+              else
+            	        System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_3);
+           }
+        } 
+	    else
+		    System.out.printf(Costanti.CONTENUTO_ELENCO_RISORSE_SOTTO_VUOTO, sc.getNome());	
+	}
+	
     public void registraPrestito(Fruitore f) 
     {
     	 Categoria c = null;
     	 SottoCategoria sc = null;
     	 Risorsa r = null;
-    	 Prestito nuovo = null;
+    	 Prestito p = null;
     	
     	 System.out.printf(Costanti.CONTENUTO_ARC, getArchivio().stampaElencoCategorie());
     	 int num1 = InputDati.leggiIntero(Costanti.INS_NUMERO_CAT_PRESTITO, Costanti.NUM_MINIMO, (getArchivio().getElencoCategorie()).size());
@@ -207,32 +272,7 @@ public class ProcessFruitoreHandler extends ProcessHandler implements Serializab
 
     	 if(c.getElencoSottoCategorie() == null)
     	 {
-      	    if(c.getElencoRisorse().size() != Costanti.VUOTO)  
-    	    {
-      	    	     System.out.printf(Costanti.CONTENUTO_CAT_RISORSA, c.getNome(), stampaElencoRisorse(c));
-    	      	 
-      	    	     if(InputDati.leggiUpperChar(Costanti.INS_PROCEDERE_PRESTITO, "SN") == 'S')
-   	    	         {
-      	    	        int num = InputDati.leggiIntero(Costanti.INS_NUMERO_RISORSA_PRESTITO, Costanti.NUM_MINIMO, c.getElencoRisorse().size());
-    	                r = c.getElencoRisorse().get(num-Costanti.NUM_MINIMO);
-    	    	
-    	                if(getArchivioPrestiti().controlloDisponibilitaRisorsa(r) && getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername()) && !(getArchivioPrestiti().verificaPresenza(r, f.getUsername())))
-  	   	                {
-  	   		               nuovo = new Prestito(c, f, r);
-  	   		               f.registraNuovoPrestito(getArchivioPrestiti(), nuovo);
-  	   		               as.getPrestitiStorici().aggiungiPrestito(nuovo);
-  	    	               System.out.println(Costanti.OP_SUCCESSO);
-  	   	                }
-  	   	                else if(!(getArchivioPrestiti().controlloDisponibilitaRisorsa(r)))
-   	                          System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_1);
-                       else if(!(getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername())))
- 	                              System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_2);
-                       else  
-               	              System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_3);
-   	    	      }
-  	    	} 
-      	    else
-      	    	    System.out.printf(Costanti.CONTENUTO_ELENCO_RISORSE_CAT_VUOTO, c.getNome());
+    		 registraPrestitoSenzaSottoCategorie(f,c,r,p);
     	 }
     	 else if((c.getElencoSottoCategorie()).size() == Costanti.VUOTO)
     	 {
@@ -240,36 +280,7 @@ public class ProcessFruitoreHandler extends ProcessHandler implements Serializab
     	 }
     	 else
     	 {
-    		System.out.printf(Costanti.CONTENUTO_CAT_SOTTO, c.getNome(), stampaElencoSottocategorie(c));
-    		int num2 = InputDati.leggiIntero(Costanti.INS_NUMERO_SOTTOC_PRESTITO, Costanti.NUM_MINIMO, (c.getElencoSottoCategorie()).size());
-    	    sc = (c.getElencoSottoCategorie()).get(num2-Costanti.NUM_MINIMO);
-    	    	    	    	    	    
-    	    if((sc.getElencoRisorse()).size() != Costanti.VUOTO)
-	        {
-	           System.out.printf(Costanti.CONTENUTO_SOTTO, sc.getNome(), stampaElencoRisorse(sc));
-	           
-	           if(InputDati.leggiUpperChar(Costanti.INS_PROCEDERE_PRESTITO, "SN") == 'S')
-	           {
-	              int num = InputDati.leggiIntero(Costanti.INS_NUMERO_RISORSA_PRESTITO, Costanti.NUM_MINIMO, sc.getElencoRisorse().size());
-	       	      r = sc.getElencoRisorse().get(num-Costanti.NUM_MINIMO);
-           
-	       	      if(getArchivioPrestiti().controlloDisponibilitaRisorsa(r) && getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername()) && !(getArchivioPrestiti().verificaPresenza(r, f.getUsername())))
-	              {
-  	   		         nuovo = new Prestito(c, f, r);
-  	   		         f.registraNuovoPrestito(getArchivioPrestiti(), nuovo);
-  	   		         as.getPrestitiStorici().aggiungiPrestito(nuovo);
-	      	         System.out.println(Costanti.OP_SUCCESSO);
-	              }
-	              else if(!(getArchivioPrestiti().controlloDisponibilitaRisorsa(r)))
-	      	                System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_1);
-	              else if(!(getArchivioPrestiti().controlloPerUlteriorePrestito(c, f.getUsername())))
-	    	                System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_2);
-	              else
-	            	        System.out.println(Costanti.OP_NO_SUCCESSO_PRESTITO_3);
-	           }
-	        } 
-    	    else
-    		    System.out.printf(Costanti.CONTENUTO_ELENCO_RISORSE_SOTTO_VUOTO, sc.getNome());	     
+    		 registraPrestitoConSottoCategorie(f,c,r,p,sc);
     	}	
     }
    
